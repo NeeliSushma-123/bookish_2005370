ad0ac76fc1143bdc4196d6e971067fe0fa20345b
NeeliSushma-123 <65879850+NeeliSushma-123@users.noreply.github.com>
2020-06-17 21:25:28 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int i,n;
+int n;
 n=5;
 unsigned fact=1;
-if(n<0)
-{
-printf("Error");
-}
-else
-{
-for(i=1;i<n;i++)
+
+for(int i=1;i<n;i++)
 {
 fact=fact*i;
 }
 printf("%u",fact);
-}
+
 return 0;
 }
